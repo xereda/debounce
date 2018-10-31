61545a9c30e1c5a37beaf59b0758b1819a67d9c8
Jackson Ricardo Schroeder <jacksons@fesctecnologia.com.br>
2018-10-31 15:58:49 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
 <template>
   <div id="app">
-    <button @click="delay">delay</button>
+    <p>{{ info }} </p>
+    <button :disabled="disabled" @click="onAction">With Delay</button>
   </div>
 </template>
 
 <script>
 import debounce from 'lodash/debounce';
 
+const DELAY_TIME = 1000;
+
 export default {
   name: 'app',
   data() {
     return {
+      disabled: false,
       delay: null,
+      info: '',
+      time: {
+        start: null,
+        end: null,
+      },
     }
   },
   created() {
-    this.delay = debounce(() => this.print(), 500);
+    this.withDelay = debounce(() => {
+      this.something();
+      this.disabled = false;
+    }, DELAY_TIME);
   },
   methods: {
-    print() {
-      console.log('disparou de dentro do debounce')
+    something() {
+      // faz alguma coisa
+      this.time = { ...this.time, end: new Date().getTime() };
+      this.info = `execution time: ${this.time.end}`;
+      console.log(this.info);
+      console.log(`delayed time: ${this.time.end - this.time.start}`);
+      console.log('===============================');
+    },
+    onAction() {
+      this.time = { ...this.time, start: new Date().getTime() };
+      console.log('===============================');
+      this.info = `start callback with delay in: ${this.time.start}`;
+      console.log(this.info);
+      this.disabled = true;
+      this.withDelay();
     },
   },
 }
